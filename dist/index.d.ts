fed29ddc0d583843530efbdeafd903bb592f44ed
ahas <dev@ahas.io>
2022-10-12 14:00:40 +0900

    chore: update version to 1.0.7

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,8 +5,8 @@ export interface MultipartOptions {
 export declare function parseValue(value: string): string | number | boolean | RegExp | any[] | Date;
 export declare function decode(obj: any): any;
 declare const _default: {
-    form: (formData: FormData, obj: any, options?: MultipartOptions) => FormData;
-    encode: (formData: FormData, obj: any, options?: MultipartOptions, lastKey?: string) => FormData;
+    form: (formData: Partial<FormData>, obj: any, options?: MultipartOptions) => Partial<FormData>;
+    encode: (formData: Partial<FormData>, obj: any, options?: MultipartOptions, lastKey?: string) => Partial<FormData>;
     decode: typeof decode;
 };
 export default _default;
